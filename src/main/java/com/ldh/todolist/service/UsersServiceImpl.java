b46ba93c2e5b213d6470fd62ea7eba4b8a83dfe2
ldh9709 <zszz5434@gmail.com>
2025-06-08 19:10:22 +0900

    chore : autowired 삭제

--- src/main/java/com/ldh/todolist/service/UsersServiceImpl.java
+++ src/main/java/com/ldh/todolist/service/UsersServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ldh.todolist.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ldh.todolist.dao.UsersDao;
