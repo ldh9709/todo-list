172c5d7470a6d155f600e944d1e122a3cecb693f
zszz9709 <zszz9709@gmail.com>
2025-06-09 10:32:31 +0900

    feat : MapperScan 어노테이션 추가

--- src/main/java/com/ldh/todolist/TodoListApplication.java
+++ src/main/java/com/ldh/todolist/TodoListApplication.java
@@ -1,9 +1,11 @@
 package com.ldh.todolist;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ldh.todolist.dao")
 public class TodoListApplication {
 
 	public static void main(String[] args) {
