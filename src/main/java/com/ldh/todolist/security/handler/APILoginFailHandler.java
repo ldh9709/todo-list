07c387efb81f317434bebb5fb57ea357481a873f
zszz9709 <zszz9709@gmail.com>
2025-06-11 17:15:14 +0900

    feat : 로그인 성공/실패 핸들러 JSP 방식으로 수정

--- src/main/java/com/ldh/todolist/security/handler/APILoginFailHandler.java
+++ src/main/java/com/ldh/todolist/security/handler/APILoginFailHandler.java
@@ -29,19 +29,6 @@ public class APILoginFailHandler implements AuthenticationFailureHandler {
 
     	log.warn("로그인 실패 - 아이디: {} / 사유: {} / IP: {} / UA: {}",
     	         username, exception.getClass().getSimpleName(), ip);
-
-        //Gson 객체 생성 → Map을 JSON 문자열로 변환하기 위해 사용
-        Gson gson = new Gson();
-
-        //실패 응답용 JSON 생성: {"error": "ERROR_LOGIN"}
-        String jsonStr = gson.toJson(Map.of("error", "ERROR_LOGIN"));
-
-        //응답 타입을 JSON으로 지정
-        response.setContentType("application/json");
-
-        //JSON 문자열을 응답으로 전송
-        PrintWriter printWriter = response.getWriter();
-        printWriter.println(jsonStr);
-        printWriter.close();
+    	response.sendRedirect("/login.jsp?error=true");
     }
 }
