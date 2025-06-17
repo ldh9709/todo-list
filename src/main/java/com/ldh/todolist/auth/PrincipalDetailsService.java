5a24cd3c6874474b9d677cee9bb11dddefd04fef
ldh9709 <zszz5434@gmail.com>
2025-06-17 11:14:25 +0900

    feat : login 컨트롤러 수정 및 디버깅 코드 추가

--- src/main/java/com/ldh/todolist/auth/PrincipalDetailsService.java
+++ src/main/java/com/ldh/todolist/auth/PrincipalDetailsService.java
@@ -19,9 +19,12 @@ public class PrincipalDetailsService implements UserDetailsService {
 	// 사용자 ID로 DB에서 사용자 정보를 조회하고, UserDetails 구현체로 반환
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		System.out.println("username : " + username);
 		
 		//아이디로 사용자 조회, 없으면 예외 처리
 		UsersDto findUsers = usersDao.findById(username);
+		
 		System.out.println("findUsers : " + findUsers);
 		return new PrincipalDetails(findUsers);
 	}
